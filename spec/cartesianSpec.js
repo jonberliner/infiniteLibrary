53db3636a41acf28d34b7b169e03b49b46240e03
Michael Chow <machow@princeton.edu>
2015-11-08 18:49:02 -0500

    added cartesian repeated test

--- spec/cartesianSpec.js
+++ spec/cartesianSpec.js
@@ -7,4 +7,9 @@ describe("Cartesian", function(){
     it("can get the final product of ['a','b'], ['x','y']", function(){
         expect(cart.cartesianN([['a', 'b'], ['x','y']], 3)).toEqual(['b', 'y']);
     });
+
+    it("can repeat a simple set", function(){
+        expect(cart.cartesianN_repeatSameSet(['a', 'b'], 3, 1))
+            .toEqual(['a', 'a', 'b']);
+    });
 });
